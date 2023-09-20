0d2ca568cdf1f286a1d8b33b09c44eb1bb4bc44c
yunchipang <yunchipang@gmail.com>
2023-09-20 14:23:34 -0700

    add dockerfile and docker-compose

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-import crud, models, schemas
+from app import crud, models, schemas
 from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
