2ede8e40227387219032c1855f6396dc2a06c55d
yunchipang <yunchipang@gmail.com>
2023-09-22 17:21:23 -0700

    rename test_read_main to test_get_root for alignment

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,7 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_get_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
