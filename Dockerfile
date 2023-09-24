fcdced25cba52332cec9eedf9bdac71f7f6c5318
yunchipang <yunchipang@gmail.com>
2023-09-23 23:02:27 -0700

    fastapi image works fine but not connected to postgres

--- Dockerfile
+++ Dockerfile
@@ -9,15 +9,10 @@ ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
 # install dependencies
-COPY requirements.txt .
-RUN pip install -r requirements.txt
+COPY requirements.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 # copy project to container
 COPY . .
 
-# expose port 8000 (FastAPI app) and 5432 (PostgreSQL)
-EXPOSE 8000
-EXPOSE 5432
-
-# Start FastAPI and PostgreSQL services
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
--- app/database.py
+++ app/database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:rena1ssance@localhost/movemakers"
+
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
+# DATABASE_URL = "postgresql://postgres:rena1ssance@localhost/movemakers"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
