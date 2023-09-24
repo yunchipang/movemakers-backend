29ae38d8394b0d00894117ddbb5d1b159448e78d
yunchipang <yunchipang@gmail.com>
2023-09-24 12:09:25 -0700

    use alembic, working on db migrations

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,16 @@
 FROM python:3.11.5-slim
 
 # set work directory
-WORKDIR /app
+WORKDIR /usr/src/app
 
 # set env variables
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-# install dependencies
-COPY requirements.txt requirements.txt
+# install python dependencies
+RUN pip install --upgrade pip
+COPY ./requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# copy project to container
+# add app
 COPY . .
-
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
--- app/database.py
+++ app/database.py
@@ -1,15 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_HOST = "db"  # Use the service name defined in Docker Compose
-
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
