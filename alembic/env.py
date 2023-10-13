6fd4c16d2e30546c41fe4ba7326fea714bcc9476
yunchipang <yunchipang@gmail.com>
2023-10-13 14:29:31 -0700

    modify alembic env.py

--- alembic/env.py
+++ alembic/env.py
@@ -20,7 +20,7 @@ config = context.config
 
 # this will overwrite the ini-file sqlalchemy.url path
 # with the path given in the config of the main code
-config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
+config.set_main_option("sqlalchemy.url", os.environ.get("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
