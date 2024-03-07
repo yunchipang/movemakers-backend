124716dbb6fbea3c7c3a75c0c291178caa354801
Yunchi Pang <yunchipang@gmail.com>
2024-03-07 11:08:31 -0800

    feature/studios (#27)
    * studios endpoints work
    
    * add unique constraint to studio name

---
+++ app/schemas/studio.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, ConfigDict
+from typing import Optional
+
+
+class BaseStudio(BaseModel):
+    name: str
+    address: str
+    email: str
+    phone: str
+    opening_hours: str
+    website: str
+    owner: Optional[str] = None
+    room_count: Optional[int] = None
+    since: Optional[int] = None
+
+class Studio(BaseStudio):
+    id: int
+
+    model_config = ConfigDict(from_attributes=True)
+
+class CreateStudio(BaseStudio):
+    pass
---
+++ app/services/studio.py
@@ -0,0 +1,51 @@
+from typing import TYPE_CHECKING, List
+
+from app.models import studio as studio_models
+from app.schemas import studio as studio_schemas
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm import Session
+
+
+# create a studio instance in database using the studio data passed in
+async def create_studio(studio: studio_schemas.CreateStudio, db: "Session") -> studio_schemas.Studio:
+    studio = studio_models.Studio(**studio.dict())
+    db.add(studio)
+    db.commit()
+    db.refresh(studio)
+    return studio_schemas.Studio.from_orm(studio)
+
+# query database to get all studios
+async def get_all_studios(db: "Session") -> List[studio_schemas.Studio]:
+    studios = db.query(studio_models.Studio).all()
+    return list(map(studio_schemas.Studio.from_orm, studios))
+
+# query database for a specific studio with the studio id
+async def get_studio(studio_id: int, db: "Session"):
+    studio = db.query(studio_models.Studio).filter(studio_models.Studio.id == studio_id).first()
+    return studio
+
+# delete a specific studio from the database
+async def delete_studio(studio: studio_models.Studio, db: "Session"):
+    db.delete(studio)
+    db.commit()
+
+# update a specific studio in the database
+async def update_studio(
+        studio_data: studio_schemas.CreateStudio, studio: studio_models.Studio, db: "Session"
+) -> studio_schemas.Studio:
+    # feed data one to one into the studio object
+    studio.name = studio_data.name
+    studio.address = studio_data.address
+    studio.email = studio_data.email
+    studio.phone = studio_data.phone
+    studio.opening_hours = studio_data.opening_hours
+    studio.website = studio_data.website
+    studio.owner = studio_data.owner
+    studio.room_count = studio_data.room_count
+    studio.since = studio_data.since
+
+    db.commit()
+    db.refresh(studio)
+
+    return studio_schemas.Studio.from_orm(studio)
