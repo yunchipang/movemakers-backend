2dfa0c1c696b7e75a9e6771e17a498e4dc326ef5
yunchipang <yunchipang@gmail.com>
2024-03-24 15:56:07 -0700

    add killable sample dancer and studio to tests

--- app/tests/test_dancers.py
+++ app/tests/test_dancers.py
@@ -1,40 +1,61 @@
-def test_get_all_dancers(test_app, sample_dancer_id):
+import pytest
+
+
+@pytest.fixture(scope="module")
+def dancer_id(test_app):
+    payload = {
+        "name": "Bada Lee 이바다",
+        "bio": "🌊🌊🌊 @teambebe_official",
+        "date_of_birth": "1995-09-22",
+        "nationality": "KR",
+        "based_in": "Seoul",
+        "instagram": "@badalee__",
+    }
+    response = test_app.post("/dancers/", json=payload)
+    assert (
+        response.status_code == 200
+    ), f"Failed to create dancer. Status code: {response.status_code}. Response body: {response.text}"
+    data = response.json()
+    return data["id"]
+
+
+def test_get_all_dancers(test_app, dancer_id):
     response = test_app.get("/dancers/")
     assert response.status_code == 200
     dancers = response.json()
     assert any(
-        dancer["id"] == sample_dancer_id for dancer in dancers
-    ), "Sample dancer not found in the list of all dancers."
+        dancer["id"] == dancer_id for dancer in dancers
+    ), "Dancer not found in the list of all dancers."
 
 
-def test_get_dancer(test_app, sample_dancer_id):
-    response = test_app.get(f"/dancers/{sample_dancer_id}")
+def test_get_dancer(test_app, dancer_id):
+    response = test_app.get(f"/dancers/{dancer_id}")
     assert response.status_code == 200
     data = response.json()
-    assert data["id"] == sample_dancer_id
+    assert data["id"] == dancer_id
 
 
-def test_update_dancer(test_app, sample_dancer_id):
-    updated_sample_payload = {
-        "youtube": "@liakimhappy",
+def test_update_dancer(test_app, dancer_id):
+    updated_payload = {
+        "youtube": "@badalee_",
     }
-    response = test_app.put(f"/dancers/{sample_dancer_id}", json=updated_sample_payload)
+    response = test_app.put(f"/dancers/{dancer_id}", json=updated_payload)
     assert response.status_code == 200
     data = response.json()
-    assert data["youtube"] == "@liakimhappy", "Dancer was not updated successfully."
+    assert data["youtube"] == "@badalee_", "Dancer was not updated successfully."
 
     # fetch the dancer to verify the update took effect
-    response = test_app.get(f"/dancers/{sample_dancer_id}")
+    response = test_app.get(f"/dancers/{dancer_id}")
     assert response.status_code == 200
     data = response.json()
-    assert data["youtube"] == "@liakimhappy", "Dancer update did not persist."
+    assert data["youtube"] == "@badalee_", "Dancer update did not persist."
 
 
-def test_delete_dancer(test_app, sample_dancer_id):
+def test_delete_dancer(test_app, dancer_id):
     # delete the dancer
-    delete_response = test_app.delete(f"/dancers/{sample_dancer_id}")
+    delete_response = test_app.delete(f"/dancers/{dancer_id}")
     assert delete_response.status_code == 200
 
     # attempt to fetch the deleted dancer
-    fetch_response = test_app.get(f"/dancers/{sample_dancer_id}")
+    fetch_response = test_app.get(f"/dancers/{dancer_id}")
     assert fetch_response.status_code == 404, "Dancer was not deleted successfully."
--- app/tests/test_studios.py
+++ app/tests/test_studios.py
@@ -1,42 +1,64 @@
-def test_get_all_studios(test_app, sample_studio_id):
+import pytest
+
+
+@pytest.fixture(scope="module")
+def studio_id(test_app, sample_dancer_id):
+    payload = {
+        "name": "1MILLION Dance Studio Daejeon",
+        "address": "56, Dunsan-ro, Seo-gu, Daejeon, Republic of Korea",
+        "email": "1mdaejeon@1milliondance.com",
+        "phone": "+82 042-488-6756",
+        "opening_hours": "Monday: 4–10 PM; Tuesday: 4–10 PM; Wednesday: 4–10 PM; Thursday: 4–10 PM; Friday: 4–10 PM; Saturday: 3–9:30 PM; Sunday: 3–9:30 PM",
+        "instagram": "@1milliondance",
+        "youtube": "@1MILLIONDanceStudioDJofficial",
+        "website": "https://www.1milliondance.com/",
+        "owner_ids": [sample_dancer_id],
+    }
+    response = test_app.post("/studios/", json=payload)
+    assert (
+        response.status_code == 200
+    ), f"Failed to create sample studio. Status code: {response.status_code}. Response body: {response.text}"
+    data = response.json()
+    return data["id"]
+
+
+def test_get_all_studios(test_app, studio_id):
     response = test_app.get("/studios/")
     assert response.status_code == 200
     studios = response.json()
     assert any(
-        studio["id"] == sample_studio_id for studio in studios
-    ), "Sample studio not found in the list of all studios."
+        studio["id"] == studio_id for studio in studios
+    ), "Studio not found in the list of all studios."
 
 
-def test_get_studio(test_app, sample_studio_id):
-    response = test_app.get(f"/studios/{sample_studio_id}")
+def test_get_studio(test_app, studio_id):
+    response = test_app.get(f"/studios/{studio_id}")
     assert response.status_code == 200
     data = response.json()
-    assert data["id"] == sample_studio_id
+    assert data["id"] == studio_id
 
 
-def test_update_studio(test_app, sample_studio_id):
-    updated_sample_payload = {"name": "(Updated) 1MILLION Dance Studio"}
-    response = test_app.put(f"/studios/{sample_studio_id}", json=updated_sample_payload)
+def test_update_studio(test_app, studio_id):
+    updated_payload = {"name": "1MILLION Dance Studio DJ"}
+    response = test_app.put(f"/studios/{studio_id}", json=updated_payload)
     assert response.status_code == 200
     data = response.json()
     assert (
-        data["name"] == "(Updated) 1MILLION Dance Studio"
+        data["name"] == "1MILLION Dance Studio DJ"
     ), "Studio was not updated successfully."
 
     # fetch the studio to verify the update took effect
-    response = test_app.get(f"/studios/{sample_studio_id}")
+    response = test_app.get(f"/studios/{studio_id}")
     assert response.status_code == 200
     data = response.json()
-    assert (
-        data["name"] == "(Updated) 1MILLION Dance Studio"
-    ), "Studio update did not persist."
+    assert data["name"] == "1MILLION Dance Studio DJ", "Studio update did not persist."
 
 
-def test_delete_studio(test_app, sample_studio_id):
+def test_delete_studio(test_app, studio_id):
     # delete the studio
-    delete_response = test_app.delete(f"/studios/{sample_studio_id}")
+    delete_response = test_app.delete(f"/studios/{studio_id}")
     assert delete_response.status_code == 200
 
     # attempt to fetch the deleted studio
-    fetch_response = test_app.get(f"/studios/{sample_studio_id}")
+    fetch_response = test_app.get(f"/studios/{studio_id}")
     assert fetch_response.status_code == 404, "Studio was not deleted successfully."
